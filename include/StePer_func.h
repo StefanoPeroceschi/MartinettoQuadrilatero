7de8521aa21dfbce390bd934f59ec8bd4fb77328
StefanoPeroceschi <noreply@stefanoperoceschi.it>
2021-04-19 10:56:58 +0200

    Aggiunto file .h con struttura quadrilatero -closes #1

---
+++ include/StePer_func.h
@@ -0,0 +1,21 @@
+#ifndef STEPER_FUNC_H
+#define STEPER_FUNC_H
+
+#include <string>
+
+
+/**
+* Struttura Quadrilatero
+* 
+* - contenente h (altezza), l (lunghezza aste), s (spessore aste) 
+*/
+struct Quadrilatero{
+    double h, l, s;
+}
+
+
+
+
+
+
+#endif // STEPER_FUNC_H
