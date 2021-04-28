0eb6e7afff47d9c400bb2f5042d927cce4011b67
StefanoPeroceschi <noreply@stefanoperoceschi.it>
2021-04-28 23:15:12 +0200

    #18 aggiunte funzioni init/check/destroy_sllevatore ed aggiunta bozza di funzione save_sollevatore

--- include/StePer_sollevatore.h
+++ include/StePer_sollevatore.h
@@ -17,13 +17,64 @@
 
 /**
 * Struttura Sollevatore
-* 
- 
+* contiene un puntatore a Scrissorlift ed uno a GuidaPrismatica 
+*  
 */
 struct StePer_Sollevatore{
-   
+   StePer_ScrissorLift* lift;
+   GuidaPrismatica* guida;
 };
 
+/**
+* Inizializzazione Sollevatore 
+* 
+* la funzione ritorna il puntatore al Sollevatore generato 
+* se i parametri sono validi altrimenti ritorna NULL
+* in ingresso chiede i seguenti parametri:
+* @param l, @param s, @param d, @param n_aste  parametri relativi al meccanismo di alzata lift
+* @param dim_blocchi , parametro relativo alla guida prismatica, in particolare alla dimensione dei blocchi quadrati
+* @param x, @param y, @param w , parametri relativi alla posizione della macchina ed alla estensione della guida prismatica alla base
+*/
+StePer_Sollevatore* StePer_init_sollevatore (
+    double l,
+    double s,
+    double d,
+    double n_aste,
+    double dim_blocchi,
+    double x,
+    double y,
+    double w
+    
+    );
+/**
+ * Deallocazione Sollevatore
+ * La funzione elimina dalla memoria un elemento della struct Sollevatore passando in ingresso un puntatore ad esso
+ * 
+ */
+void StePer_destroy_sollevatore(
+
+    StePer_Sollevatore* sol
+
+    );
+
+
+/**
+ * Controllo Sollevatore
+ * La funzione verifica che i parametri dei due componenti di sollevatore siano compatibili 
+ * ritorna 0 se sono compatibili, 1 altrimenti
+ */
+int StePer_check_sollevatore(
+
+    double l,
+    double s,
+    double d,
+    double n_aste,
+    double dim_blocchi,
+    double x,
+    double y,
+    double w
+    
+    );
 
 
 #endif 
